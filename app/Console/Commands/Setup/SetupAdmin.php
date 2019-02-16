716f2716be829b52a66314865824a83989f44da1
rully <hvsciverius@gmail.com>
2019-02-16 17:32:49 +0700

    add setup admin command

---
+++ app/Console/Commands/Setup/SetupAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands\Setup;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class SetupAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $adminName = $this->ask("Admin name");
+        $adminEmail = $this->ask("Admin email");
+        $adminPassword = $this->secret("Admin password");
+
+        if (User::where('email', $adminEmail)->count() > 0)
+        {
+            $this->error(__("Email address already exists!"));
+            return;
+        }
+
+        $admin = User::create([
+            'name' => $adminName,
+            'email' => $adminEmail,
+            'password' => \Hash::make($adminPassword),
+        ]);
+
+        $this->info(__("Admin account created successfully"));
+    }
+}
